752dc14a1122db6e6922356abfdcd274b36a8caf
sven.zhu <gy1zhuxiwen@gmail.com>
2020-06-03 19:02:36 +0800

    Init

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+#install necessary applications
+sudo apt install -y git vim
+
+#config zsh
+sudo apt install -y zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+#config tmux
+sudo apt install -y tmux
+cd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
