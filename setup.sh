573c105d87dd1b5a84e37660af47df0c4b572c00
sven.zhu <gy1zhuxiwen@gmail.com>
2020-07-09 00:35:52 +0800

    add docker

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+sudo apt update
+
 #install necessary applications
 sudo apt install -y git vim
 
@@ -8,7 +10,6 @@ sudo apt install -y zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
 #config tmux
 sudo apt install -y tmux
 cd ~
@@ -16,3 +17,26 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+#config docker
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
